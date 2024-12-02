789a5fe827b2cad0fbcbb46af18b6651bb9dcd10
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-12-02 15:37:05 -0300

    feat: adiciona campos ao formulário de produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
-export class CreateComponent {
 
+export class CreateComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('', { 
+      nonNullable: true, 
+      validators: Validators.required
+    }),
+  });
+
+  onSubmit() {
+    this.form.controls.title.value;
+  }
 }
