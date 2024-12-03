1b2a232f9a106d504cd39db5b90f31c8b6ae764a
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-12-03 16:02:52 -0300

    feat: adiciona componente para navegar para listagem

---
+++ src/app/shared/components/back-to-list/back-to-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-back-to-list',
+  imports: [RouterLink, MatButtonModule],
+  templateUrl: './back-to-list.component.html',
+  styleUrl: './back-to-list.component.scss'
+})
+export class BackToListComponent {
+
+}
